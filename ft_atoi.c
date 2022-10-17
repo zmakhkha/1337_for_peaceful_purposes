6088e39501975658c2f7375789ef56bee0c14757
zmksiii <jcaladic@gmail.com>
2022-10-17 19:51:38 +0100

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,20 +6,11 @@
 /*   By: zmakhkha <zmakhkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 14:21:30 by zmakhkha          #+#    #+#             */
-/*   Updated: 2022/10/16 15:19:09 by zmakhkha         ###   ########.fr       */
+/*   Updated: 2022/10/17 19:03:57 by zmakhkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-
-static spaces(int d)
-{
-    char c;
-    c = (unsigned char)d;
-    if(c == ' ')
-        return(1);
-    return (0);
-}
 
 static int ft_isdigit(int c){
         c = (unsigned char) c;
@@ -29,27 +20,46 @@ static int ft_isdigit(int c){
             return (0);
     }
 
+#include<stdio.h>
 int ft_atoi(const char *str)
 {
     char *tmp = (char*) str;
     int i;
     int j;
+    int sign;
+    int res;
+    sign = 1;
     i=0;
-    while(spaces(tmp[i]))
-    {
+    res =0;
+    while(tmp[i] == ' ')
         i++;
-        if(tmp[i] == '+')
+    while(ft_isdigit(tmp[i]))
         {
+            res += tmp[i]-48 ;
+            if(ft_isdigit(tmp[i+1]))
+                res = res*10;
+            if (!ft_isdigit(tmp[i+1]))
+                return(res);
+            i++;
+        }
+    if(((tmp[i] == '+') ||(tmp[i] == '-')) && ft_isdigit(tmp[i +1]))
+        {
+            if(tmp[i] == '-')
+                sign = -1;
             j = i+1;
             while(ft_isdigit(tmp[j]))
-                
-        } 
-    }
+                {   res += tmp[j]-48 ;
+                    if(ft_isdigit(tmp[j+1]))
+                        res = res*10;
+                    j++;
+                }
+            return (res * sign);    
+        }
     return(0);
 }
-#include<stdio.h>
 int main()
 {
-    printf("%d", atoi(NULL));
+    printf("%d\n\n", atoi("gra-4888-12"));
+    printf("%d", ft_atoi("gra-4888-12"));
     return(0);
 }
