b25cea55a1b09c3a4bed4d8c9c9d37387f17aa9d
zmksiii <jcaladic@gmail.com>
2022-10-20 22:07:40 +0100

    ft_split

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,10 +6,11 @@
 /*   By: zmakhkha <zmakhkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 12:07:14 by zmakhkha          #+#    #+#             */
-/*   Updated: 2022/10/18 17:04:29 by zmakhkha         ###   ########.fr       */
+/*   Updated: 2022/10/20 21:46:16 by zmakhkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include<string.h>
 char *ft_strchr(const char *s, int c)
 {
     int i;
@@ -17,7 +18,13 @@ char *ft_strchr(const char *s, int c)
     c = (char)c;
     while((s[i] != '\0') && (s[i] != c))
         i++;
+    if(s[i] == c)
     return((char*)&s[i]);
+
+    if(s[i] == c)
+        return((char*)&s[i]);
+    if(s[i] == '\0')
+        return(NULL);
 }
 #include<string.h>
 #include<stdio.h>
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: zmakhkha <zmakhkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 16:50:34 by zmakhkha          #+#    #+#             */
-/*   Updated: 2022/10/18 20:59:42 by zmakhkha         ###   ########.fr       */
+/*   Updated: 2022/10/20 19:23:23 by zmakhkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,45 +33,59 @@ size_t  ft_strlen(const char *s){
 #include<stdio.h>
 char *ft_strtrim(char const *s1, char const *set)
 {
-    if(!s1)
-        return (NULL);
-    char *s;
-    char *trim;
-    s = (char*)s1;
-    trim = (char*)set;
-    int i, len1, len2, j;
-    i=0;j=0;
-    len1 = ft_strlen(trim);
-    len2 = ft_strlen(s);
-
-    i = len2 - len1;
-
-    while ((i < len2) && (trim[i] != '\0') && (s[i] == trim[i]))
+    int size_s1;
+    int size_trim;
+    char *tmp;
+    tmp = (char*) s1;
+    size_s1 = ft_strlen(s1);
+    size_trim = ft_strlen(set);
+    int size_res;
+    if(size_s1 >= size_trim)
+    {
+        int i;
+        i =0;
+        while(tmp[size_s1-size_trim +i] == set[i])
             i++;
-
-    while ((i < len2) && (trim[i] != '\0') && (s[i] == trim[i]))
-        i++;
-    if (trim[i] == '\0')
-        {
-            int j =0;
-            int l1 = ft_strlen(s+i);
-            // printf("hadi l1: %d", l1);
-            char *k = (char*)malloc(l1+1);
-            if (!k)
-                return (NULL);
-            j =0;
-            while (j <l1)
-                {
-                    k[j]= s[i];
-                    i++;
+        if(set[i] == '\0')
+            {
+                tmp[size_s1-size_trim +i] = '\0';
+                int k = ft_strlen(tmp);
+                size_res = k - size_trim +1;
+            }
+        i =0;
+        while(tmp[i] == set[i])
+            i++;
+        if(set[i] == '\0')
+            {
+                int k = ft_strlen(tmp);
+                size_res = k - size_trim +1;
+                char *res = (char*)malloc(size_res);
+                    return(NULL);
+                int j=0;
+                while (tmp[j] != '\0')
+                {    
+                    res[j] = tmp[size_trim +j];
                     j++;
                 }
-            k[i] = '\0';
-            return(k);
+                res[j] = '\0';
+            }
+        char *res = (char*)malloc(size_res);
+        if(!res)
+            return(NULL);
+        int j=0;
+        while (tmp[j] != '\0')
+        {    
+            res[j] = tmp[size_trim +j];
+            j++;
         }
+        res[j] = '\0';
+        return(res);
+    }
+    else
+        return(NULL);
 }
 int main()
 {
     // printf("%lu", ft_strlen(ft_strtrim("abchgig", "abc")));
-    printf("%s", ft_strtrim("abhchgig", "abc"));
+    printf("%s", ft_strtrim("hchgigab", "ab"));
 }
